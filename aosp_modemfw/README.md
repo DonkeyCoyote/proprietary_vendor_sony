72bc4fef6e7338a730e94cd143b88ed514afe66f
Andrew Dodd <atd7@cornell.edu>
2015-04-18 14:15:48 -0400

    Sony AOSP devices: Reorganize modem firmware
    Thanks to the SuperBenevolent One for this

---
+++ aosp_modemfw/README.md
@@ -0,0 +1,19 @@
+# Vendor blobs for AOSP Lollipop
+
+### Shinano
+* leo      - 23.1.A.0.690 (D6603)
+* aries    - 23.1.A.0.690 (D5803)
+* scorpion -
+* sirius   - 23.1.A.0.690 (D6503)
+* castor   -
+
+### Rhine
+* honami   - 14.4.A.0.157
+* amami    - 14.4.A.0.108
+* togari   - 14.4.A.0.108
+
+### Yukon
+* tianchi  - 19.1.A.0.473
+* seagull  -
+* eagle    -
+* flamingo - 18.4.C.2.12
---
+++ aosp_modemfw/honami/honami-partial.mk
@@ -0,0 +1,36 @@
+# Copyright (C) 2015 SuperBenevolent
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+PRODUCT_PACKAGES += \
+	modem_b00 \
+	modem_b01 \
+	modem_b02 \
+	modem_b03 \
+	modem_b04 \
+	modem_b05 \
+	modem_b08 \
+	modem_b11 \
+	modem_b13 \
+	modem_b14 \
+	modem_b15 \
+	modem_b16 \
+	modem_b17 \
+	modem_b18 \
+	modem_b19 \
+	modem_b20 \
+	modem_b21 \
+	modem_b22 \
+	modem_b25 \
+	modem_b26 \
+	modem_mdt

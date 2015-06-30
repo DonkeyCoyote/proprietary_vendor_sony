d062c64d1bc2c5203332d7ca710541e102547565
Myself5 <myself5@carbonrom.org>
2015-06-30 14:34:03 -0400

    sirius, aries and leo: Fix Modem after V9 update
    I called the wrong qcom mk file before,
    which resulted in the radio blobs not getting implemented.

--- aosp_qcom/prebuilt/qcom-vendor.mk
+++ aosp_qcom/prebuilt/qcom-vendor.mk
@@ -1,2 +1,2 @@
-$(call inherit-product, vendor/sony/aosp_qcom/prebuilt/qcom-partial.mk)
+$(call inherit-product, vendor/sony/aosp_qcom/prebuilt/qcom-partial-32bit.mk)
 $(call inherit-product-if-exists, vendor/sony/aosp_modemfw/qcom-firmware.mk)
--- aries/aries-vendor.mk
+++ aries/aries-vendor.mk
@@ -1,6 +1,5 @@
 PRODUCT_PLATFORM := shinano2
-$(call inherit-product, vendor/sony/aosp_qcom/prebuilt/qcom-partial-32bit.mk)
+$(call inherit-product, vendor/sony/aosp_qcom/prebuilt/qcom-vendor.mk)
 $(call inherit-product, vendor/sony/shinano/firmware-partial.mk)
 $(call inherit-product, vendor/sony/shinano/shinano-partial.mk)
-$(call inherit-product, vendor/sony/aosp_modemfw/aries/aries-partial.mk)
 $(call inherit-product, $(LOCAL_PATH)/aries-partial.mk)

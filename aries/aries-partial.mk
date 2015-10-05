6ff9e823e498574bfec357ce95510dc0464d94a8
Myself5 <myself5@carbonrom.org>
2015-10-05 15:17:23 -0400

    vendor: Intial Aries bringup
    Uses the Sony V11 Blobs

---
+++ aries/aries-partial.mk
@@ -0,0 +1,32 @@
+# Copyright (C) 2012 Sony Mobile Communication
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+PRODUCT_PACKAGES += \
+	Handset_cal \
+	Speaker_cal \
+	touch_module_id_0x82 \
+	touch_module_id_0x84 \
+	touch_module_id_0x85 \
+	touch_module_id_0x87 \
+	touch_module_id_0x88 \
+	touch_module_id_0x89 \
+	touch_module_id_0x8a \
+	touch_module_id_0x8c \
+	touch_module_id_0x8d \
+	touch_module_id_0x8e \
+	touch_module_id_0x92 \
+	touch_module_id_0x93 \
+	touch_module_id_0x94 \
+	fw_bcmdhd \
+	fw_bcmdhd_apsta

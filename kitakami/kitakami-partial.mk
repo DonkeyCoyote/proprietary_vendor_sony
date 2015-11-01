1118190f39d58b7582c608184077e84ded700c45
Humberto Borba <humberos@gmail.com>
2015-11-01 21:53:36 -0200

    sony: aosp: Add kitakami's karin
    Signed-off-by: Humberto Borba <humberos@gmail.com>

---
+++ kitakami/kitakami-partial.mk
@@ -0,0 +1,70 @@
+# Copyright (C) 2012 Sony Mobile Communication
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+PRODUCT_PACKAGES += \
+	Bluetooth_cal \
+	General_cal \
+	Global_cal \
+	Hdmi_cal \
+	Headset_cal \
+	cmnlib_b00 \
+	cmnlib_b01 \
+	cmnlib_b02 \
+	cmnlib_b03 \
+	cmnlib_mdt \
+	tzdtcpkctl_b00 \
+	tzdtcpkctl_b01 \
+	tzdtcpkctl_b02 \
+	tzdtcpkctl_b03 \
+	tzdtcpkctl_mdt \
+	tzhdcp_b00 \
+	tzhdcp_b01 \
+	tzhdcp_b02 \
+	tzhdcp_b03 \
+	tzhdcp_mdt \
+	tzlibasb_b00 \
+	tzlibasb_b01 \
+	tzlibasb_b02 \
+	tzlibasb_b03 \
+	tzlibasb_mdt \
+	tznautilus_b00 \
+	tznautilus_b01 \
+	tznautilus_b02 \
+	tznautilus_b03 \
+	tznautilus_mdt \
+	tzsuntory_b00 \
+	tzsuntory_b01 \
+	tzsuntory_b02 \
+	tzsuntory_b03 \
+	tzsuntory_mdt \
+	tztpm_b00 \
+	tztpm_b01 \
+	tztpm_b02 \
+	tztpm_b03 \
+	tztpm_mdt \
+	tzwidevine_b00 \
+	tzwidevine_b01 \
+	tzwidevine_b02 \
+	tzwidevine_b03 \
+	tzwidevine_mdt \
+	venus_b00 \
+	venus_b01 \
+	venus_b02 \
+	venus_b03 \
+	venus_b04 \
+	venus_mdt \
+	libpn547_fw \
+	sensors_msm8994 \
+	sensors_oem \
+	libadsprpc \

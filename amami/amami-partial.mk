6be40806a338bdc49826da9e91358090582d92b0
Erik Castricum <erikcas1972@gmail.com>
2016-02-08 22:53:51 +0100

    amami: add firmware Android M v02a
    Signed-off-by: Erik Castricum <erikcas1972@gmail.com>

---
+++ amami/amami-partial.mk
@@ -0,0 +1,24 @@
+# Copyright (C) 2012 Sony Mobile Communication
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+PRODUCT_PACKAGES += \
+	Handset_cal \
+	Speaker_cal \
+	touch_module_id_0x45 \
+	touch_module_id_0x48 \
+	touch_module_id_0x4d \
+	touch_module_id_0x4e \
+	touch_module_id_0x53 \
+	touch_module_id_0x54 \
+	touch_module_id_0x5d

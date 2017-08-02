26216f7996f8abdc092a939c18f57f3bcf2d1254
oshmoun <oshmoun100@googlemail.com>
2017-08-03 00:36:21 +0200

    aosp: loire: update blobs to release V12
    Change-Id: I5b8e62c26316c7044831fb542bddba69d1424a24

--- loire-kugo/kugo-vendor.mk
+++
@@ -1,5 +0,0 @@
-PRODUCT_PLATFORM := loire
-$(call inherit-product, vendor/sony/aosp_nxp/nxp-vendor.mk)
-$(call inherit-product, vendor/sony/aosp_qcom/prebuilt/qcom-vendor.mk)
-$(call inherit-product, vendor/sony/loire-common/loire-partial.mk)
-$(call inherit-product, $(LOCAL_PATH)/kugo-partial.mk)
